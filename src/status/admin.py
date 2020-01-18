98c92c61f3600d83ba931a11d37949f36e7ffa0c
jihoon <blue_jihoon@naver.com>
2020-01-18 16:35:45 +0900

    model form for validation

--- src/status/admin.py
+++ src/status/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from status.forms import StatusForm
 from status.models import Status
 
-admin.site.register(Status)
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['user', '__str__', 'image']
+    # form을 등록함으로써 admin에서 데이터를 입력할 때 validation을 추가할 수 있다.
+    form = StatusForm
+
+    # class Meta:
+    #     model = Status
+
+
+admin.site.register(Status, StatusAdmin)
