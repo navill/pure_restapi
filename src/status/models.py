a1a90954bfe45f43c4fcbc7be582b07539eaf2c8
jihoon <blue_jihoon@naver.com>
2020-01-18 16:27:22 +0900

    new app - status

--- src/status/models.py
+++ src/status/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+def upload_status_image(instance, filename):
+    return f'updates/{instance.user}/{filename}'
+
+
+class StatusQuerySet(models.QuerySet):
+    pass
+
+
+class StatusManager(models.Manager):
+    def get_queryset(self):
+        return StatusQuerySet(self.model, using=self._db)
+
+
+class Status(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to=upload_status_image, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    objects = StatusManager()
+
+    def __str__(self):
+        return str(self.content)[:50]
